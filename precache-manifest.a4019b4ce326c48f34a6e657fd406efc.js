462907ff0a905bfbcc63c1c2f8a2f549c6a9e62b
Anolla <runner@fv-az71.5wa55nr20cfujczyxphmgmtbmd.bx.internal.cloudapp.net>
2020-06-29 19:50:10 +0000

    Updates

---
+++ precache-manifest.a4019b4ce326c48f34a6e657fd406efc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2c16f3cbfcac9f9818ca0284b5f7d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9447871de0aab476d830",
+    "url": "/static/css/main.d7651146.chunk.css"
+  },
+  {
+    "revision": "f1b9fe14f2afe522a17b",
+    "url": "/static/js/2.2c2e7c3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c2e7c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9447871de0aab476d830",
+    "url": "/static/js/main.37d8c190.chunk.js"
+  },
+  {
+    "revision": "411f183aa86445d317cd",
+    "url": "/static/js/runtime-main.8eb541cb.js"
+  }
+]);
