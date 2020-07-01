20d4cc12d29362811159499a1011254b623e895f
Anolla <annehaddad96@yahoo.com>
2020-07-01 23:45:54 +0300

    someCss

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
     return (
       <header>
         <h1>RESTy</h1>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/history">History</NavLink>
+            </li>
+          </ul>
+        </nav>
       </header>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
+import { BrowserRouter } from 'react-router-dom';
 
 class Main extends React.Component {
   render() {
     return (
-      <React.StrictMode>
-        <App />;
-      </React.StrictMode>
+      <BrowserRouter>
+        <React.StrictMode>
+          <App />;
+        </React.StrictMode>
+      </BrowserRouter>
     );
       
   }
